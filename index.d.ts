3376d634ab714c41fa119e0298fd0a75e48fe74c
DiamondYuan <541832074@qq.com>
2019-02-18 10:55:21 +0800

    docs: add index.d.ts and test case (#4)

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+import { WriteStream } from 'fs';
+import { ChildProcess } from 'child_process';
+
+type LogLevel =
+  | 'DEBUG'
+  | 'INFO'
+  | 'LOG'
+  | 'WARN'
+  | 'ERROR'
+  | 'NONE'
+  | 'ALL'
+  | number;
+
+interface ConsoleLoggerOpt {
+  stdout?: WriteStream;
+  stderr?: WriteStream;
+  prefix?: string;
+  time?: boolean;
+  level?: LogLevel;
+}
+
+declare class ConsoleLogger extends Console {
+  constructor(opt?: ConsoleLoggerOpt);
+
+  private stdout:WriteStream;
+
+  private stderr:WriteStream;
+
+  private level:LogLevel;
+
+  child(prefix: string): ConsoleLogger;
+
+  child(child: ChildProcess | WriteStream, prefix: string): ConsoleLogger;
+
+  end(): void;
+}
+
+export = ConsoleLogger;
---
+++ test/fixtures/ts/test.ts
@@ -0,0 +1,30 @@
+import ConsoleLogger = require('../../../.');
+import * as child_process from 'child_process';
+import * as fs from 'fs';
+
+let logger = new ConsoleLogger({
+  stdout: fs.createWriteStream('stdout.log'),
+  stderr: fs.createWriteStream('stderr.log'),
+  prefix: '> ',
+  level: 'WARN',
+  time: false,
+});
+
+logger.debug('debug', 'debug');
+logger.log('log', 'log');
+logger.info('info', 'info');
+logger.warn('warn', 'warn');
+logger.error('error', 'error');
+
+let child = logger.child('child> ');
+const ls = child_process.spawn('ls', { cwd: __dirname });
+child = logger.child(ls, 'child> ');
+child.debug('debug', 'debug');
+child.log('log', 'log');
+child.info('info', 'info');
+child.warn('warn', 'warn');
+child.error('error', 'error');
+
+logger = new ConsoleLogger();
+
+logger.end();
